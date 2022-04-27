2b8e3b9ad097dc112ec93519db128b7dc24b8c3e
MarShallOwn <marwansamih15@gmail.com>
2022-04-28 00:22:18 +0200

    fixing changes in back-end sh

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb init udagram-api --platform "Node.js 16 running ob 64bit Amazon Linux 2" --region us-east-1 &&
+eb init udagram-api --platform "Node.js 16 running on 64bit Amazon Linux 2" --region us-east-1 &&
 eb use Udagram-api-env && eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB POSTGRES_PORT=$POSTGRES_PORT POSTGRES_HOST=$POSTGRES_HOST JWT_SECRET=$JWT_SECRET &&
 eb deploy Udagram-api-env
