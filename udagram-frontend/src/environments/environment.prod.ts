33345749027d3698e056c78b6f9613c8b4905f75
MarShallOwn <marwansamih15@gmail.com>
2022-04-28 01:42:36 +0200

    update back-end endpoint that was stored in the front-end

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-env.eba-mjvuhf88.us-east-1.elasticbeanstalk.com/",
+  apiHost: "http://udagram-api-env.eba-mjvuhf88.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
